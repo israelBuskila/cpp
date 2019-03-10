48ff4b57c624529982df2c026b3d6fa02a43cbd9
israelBuskila <israelad3@gmail.com>
2019-03-11 00:37:53 +0200

    test program by my script

---
+++ BasicCheck.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+folderName=$1
+executble=$2
+currentLocation=`pwd`
+
+cd $folderName
+make >> /dev/null 2>&1
+executMake=$?
+if [ $executMake -ne 0 ]; then
+echo Makefile did not found
+echo 7
+exit 7
+fi
+
+valgrind --leak-check=full --error-exitcode=1 >> /dev/null 2>&1 ./$executble 
+val=$?
+erval=PASS
+if [ $val -eq 1 ]; then
+erval=FAIL
+fi
+
+
+valgrind --tool=helgrind --error-exitcode=1 >> /dev/null 2>&1 ./$executble
+hel=$?
+erhel=PASS
+if [ $hel -eq 1 ]; then
+erhel=FAIL
+fi
+
+
+echo "compilation," "Memory leaks," "Thread race"
+echo  "   "PASS"       " $erval "        " $erhel
+
+ans=0
+if [ $val -eq 1 ] && [ $hel -eq 1 ]; then
+ans=3
+fi
+if [ $val -ne 1 ] && [ $hel -eq 1 ]; then
+ans=1
+fi
+if [ $val -eq 1 ] && [ $hel -ne 1 ]; then
+ans=2
+fi
+
+echo $ans
